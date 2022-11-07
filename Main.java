e8fd5a2c248510ca21630eda7cdcb31255200781
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-07 16:52:30 +0000

    Setup first For Loop

--- Main.java
+++ Main.java
@@ -22,6 +22,14 @@ class Main {
   double[] medicarePercentage = {1.45, 2.35};
   double[] medicateFlat = {0,2900};
 
+  int fedBracket = 0;
+    
+  for(int i = 0; i < federalThreshold.length; i++){
+    if(federalThreshold[i] >=salary){
+      fedBracket++;
+    }
+  }
+
   
   
   }
