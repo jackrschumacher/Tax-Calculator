1d47154234129298f0d74f4c76743cb87a13ed53
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-07 15:04:33 +0000

    Add thresholds

--- Main.java
+++ Main.java
@@ -2,32 +2,18 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    /*Wisconsin State Tax Brackets:
-    * 0-12760 -- 3.54% -- $0  
-    * 12761 - $24420 -- 4.65 --$451.70
-    * $25521 - $280950 -- 7.65% -- $14582.83
-    * $280950 and over -- 7.65% -- $14582.83
-    
 
-    Federal State Tax Brackets:
-    *10% -- $0 to $10,275.
-    *12% -- $10,276 to $41,775. -- $1,027.50
-    *22% -- $41,776 to $89,075. -- $4,807.50
-    *24% --$89,076 to $170,050. -- $15,213.50
-    *32% -- $170,051 to $215,950. -- $34,647.50
-    *35% -- $215,951 to $539,900. -- $49,335.50
-    *37% -- $539,901 or more. --$162,718
+  System.out.println("How much money do you make each year?");
+  double salary = input.nextDouble();
 
-    Social Seurity Tax:
-    * $0- $147000 -- 6.2% -- $0
-    * $147001 and over -- 0% -- $9114
+  double stateThreshold = {0, 12761, 25521, 280950}
+  double statePercentage = {3.54, 4.65, 7.65, 7.65}
+  double stateFlat = {0, 451,70, 14582.83, 14582.83}
 
-    Medicare Tac
-    * $0 - $200000 -- 1.45% -- 0%
-    * $2000001 and over  -- 2.35% -- $2900
-    */
-
-  System.out.println("How much money do make each year?");
+  double federalThreshold = {0, 10276, 41776, 89076, 170051, 215951, 539901}
+  double federalPercentage = {10, 12, 22, 24, 32, 35, 37}
   
-    
+
+  
+  }
 }
