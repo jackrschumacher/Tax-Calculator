cc33e86dec28e56d00c1f48ec4741e58ffd86e05
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-07 16:48:53 +0000

    Finish Thresholds

--- Main.java
+++ Main.java
@@ -6,15 +6,23 @@ class Main {
   System.out.println("How much money do you make each year?");
   double salary = input.nextDouble();
 
-  double stateThreshold = {0, 12761, 25521, 280950}
-  double statePercentage = {3.54, 4.65, 7.65, 7.65}
-  double stateFlat = {0, 451,70, 14582.83, 14582.83}
+  double[] stateThreshold = {0, 12761, 25521, 280950};
+  double[] statePercentage = {3.54, 4.65, 7.65, 7.65};
+  double[] stateFlat = {0, 451,70, 14582.83, 14582.83};
 
-  double federalThreshold = {0, 10276, 41776, 89076, 170051, 215951, 539901}
-  double federalPercentage = {10, 12, 22, 24, 32, 35, 37}
-  double federalFlat = {1027.50, 4807.50, 15213.50, 34647.50, 49355.50, 162718}
+  double[] federalThreshold = {0, 10276, 41776, 89076, 170051, 215951, 539901};
+  double[] federalPercentage = {.1, .12, .22, .24, .32, .35, .37};
+  double[] federalFlat = {1027.50, 4807.50, 15213.50, 34647.50, 49355.50, 162718};
+
+  double[] socialSecurityThreshold = {0, 1470001};
+  double[] socialSecurityPercentage = {0.62, 0};
+  double[] socialSecurityFlat = {0, 9114};
+
+  double[] medicareTax = {0, 2000001};
+  double[] medicarePercentage = {1.45, 2.35};
+  double[] medicateFlat = {0,2900};
+
   
-
   
   }
 }
